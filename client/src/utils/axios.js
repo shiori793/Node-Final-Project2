f33996370b142ad3e474756becab8b7e1676cc29
Shiori <shiori.fujii.cb@gmail.com>
2023-07-19 16:30:52 -0700

    add axios default header

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = "https://blog-app-api-ir8o.onrender.com";
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = "https://blog-app-api-ir8o.onrender.com";
 
 export default axios;
